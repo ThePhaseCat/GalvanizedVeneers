43764b14495730888aeabcfb7422a104783839ac
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-23 09:17:48 -0600

    added block tag and recipe data gen

--- src/main/java/phase/galvanized_veneers/GalvanizedVeneersDataGenerator.java
+++ src/main/java/phase/galvanized_veneers/GalvanizedVeneersDataGenerator.java
@@ -3,11 +3,15 @@ package phase.galvanized_veneers;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import phase.galvanized_veneers.datagen.BlockLootTableGen;
+import phase.galvanized_veneers.datagen.BlockTagGen;
+import phase.galvanized_veneers.datagen.RecipeGen;
 
 public class GalvanizedVeneersDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack myPack = fabricDataGenerator.createPack();
 		myPack.addProvider(BlockLootTableGen::new);
+		myPack.addProvider(BlockTagGen::new);
+		myPack.addProvider(RecipeGen::new);
 	}
 }
---
+++ src/main/java/phase/galvanized_veneers/datagen/RecipeGen.java
@@ -0,0 +1,31 @@
+package phase.galvanized_veneers.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+import phase.galvanized_veneers.GalvanizedVeneers;
+
+public class RecipeGen extends FabricRecipeProvider
+{
+    public RecipeGen(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture)
+    {
+        super(dataOutput, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, GalvanizedVeneers.GALVANIZED_SQUARE_STEEL)
+                .pattern("bbb").pattern("b b").pattern("bbb")
+                .input('b', Items.IRON_INGOT)
+                .criterion(FabricRecipeProvider.hasItem(Items.IRON_INGOT),
+                        FabricRecipeProvider.conditionsFromItem(Items.IRON_INGOT))
+                .offerTo(exporter);
+    }
+}
