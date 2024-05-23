a83de980f13f3a3a08ba8bf9afb29586a5a12f93
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-22 19:48:22 -0600

    added galvanized square steel
    little john would be so happy

--- src/main/java/phase/galvanized_veneers/GalvanizedVeneers.java
+++ src/main/java/phase/galvanized_veneers/GalvanizedVeneers.java
@@ -2,6 +2,12 @@ package phase.galvanized_veneers;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +17,16 @@ public class GalvanizedVeneers implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("galvanizedveneers");
 
+	public static final Block GALVANIZED_SQUARE_STEEL = new Block(Block.Settings.create().strength(10.0f));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("GalvanizedVeneers Started");
+		Registry.register(Registries.BLOCK, new Identifier("galvanizedveneers", "galvanized_square_steel"), GALVANIZED_SQUARE_STEEL);
+		Registry.register(Registries.ITEM, new Identifier("galvanizedveneers", "galvanized_square_steel"), new BlockItem(GALVANIZED_SQUARE_STEEL, new Item.Settings()));
 	}
 }
