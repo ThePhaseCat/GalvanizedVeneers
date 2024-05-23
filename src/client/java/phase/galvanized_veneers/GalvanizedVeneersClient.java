284ae0f28bb3350bf1442e5b7153a3e24ce20624
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-22 19:31:41 -0600

    hi

---
+++ src/client/java/phase/galvanized_veneers/GalvanizedVeneersClient.java
@@ -0,0 +1,10 @@
+package phase.galvanized_veneers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GalvanizedVeneersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/phase/galvanized_veneers/GalvanizedVeneersDataGenerator.java
@@ -0,0 +1,11 @@
+package phase.galvanized_veneers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GalvanizedVeneersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
