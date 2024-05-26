fc4686350427e78de69fd61bfd4d3eaa125d2795
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-26 09:58:27 -0600

    music disc started

--- src/main/java/phase/galvanized_veneers/GalvanizedVeneers.java
+++ src/main/java/phase/galvanized_veneers/GalvanizedVeneers.java
@@ -5,12 +5,10 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
@@ -22,8 +20,12 @@ public class GalvanizedVeneers implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("galvanizedveneers");
 
+	private static SoundEvent little_john = registerSoundEvent("little_john");
+
 	public static final Block GALVANIZED_SQUARE_STEEL = new Block(Block.Settings.create().hardness(1.5f).resistance(100.0f));
 	public static final Block WOOD_VENEER = new Block(Block.Settings.create().hardness(0.5f).resistance(2.5f));
+
+	public static final Item LITTLE_JOHN_DISC = new MusicDiscItem(15, little_john, new Item.Settings().maxCount(1), 48);
 
 	private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
 			.icon(() -> new ItemStack(GALVANIZED_SQUARE_STEEL))
@@ -31,8 +33,11 @@ public class GalvanizedVeneers implements ModInitializer {
 			.entries((context, entries) -> {
 				entries.add(GALVANIZED_SQUARE_STEEL.asItem());
 				entries.add(WOOD_VENEER.asItem());
+				entries.add(LITTLE_JOHN_DISC);
 			})
 			.build();
+
+
 
 	@Override
 	public void onInitialize() {
@@ -45,7 +50,13 @@ public class GalvanizedVeneers implements ModInitializer {
 		Registry.register(Registries.ITEM, new Identifier("galvanizedveneers", "galvanized_square_steel"), new BlockItem(GALVANIZED_SQUARE_STEEL, new Item.Settings()));
 		Registry.register(Registries.BLOCK, new Identifier("galvanizedveneers", "wood_veneer"), WOOD_VENEER);
 		Registry.register(Registries.ITEM, new Identifier("galvanizedveneers", "wood_veneer"), new BlockItem(WOOD_VENEER, new Item.Settings()));
+		Registry.register(Registries.ITEM, new Identifier("galvanizedveneers", "little_john_disc"), LITTLE_JOHN_DISC);
 
 		Registry.register(Registries.ITEM_GROUP, new Identifier("galvanizedveneers", "item_group"), ITEM_GROUP);
+	}
+
+	private static SoundEvent registerSoundEvent(String name) {
+		Identifier id = new Identifier("galvanizedveneers", name);
+		return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
 	}
 }
--- src/main/java/phase/galvanized_veneers/datagen/BlockTagGen.java
+++ src/main/java/phase/galvanized_veneers/datagen/BlockTagGen.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 import phase.galvanized_veneers.GalvanizedVeneers;
 
 import java.util.concurrent.CompletableFuture;
@@ -19,5 +20,11 @@ public class BlockTagGen extends FabricTagProvider.BlockTagProvider{
         getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
                 .add(GalvanizedVeneers.GALVANIZED_SQUARE_STEEL)
                 .add(GalvanizedVeneers.WOOD_VENEER);
+
+        getOrCreateTagBuilder(ItemTags.MUSIC_DISCS)
+                .add(ModItems.BAR_BRAWL_MUSIC_DISC);
+
+        getOrCreateTagBuilder(ItemTags.CREEPER_DROP_MUSIC_DISCS)
+                .add(ModItems.BAR_BRAWL_MUSIC_DISC);
     }
 }
