e72188e6c56456759a04bf95933f87becd9936f7
ThePhaseCat <alexmgamer10@gmail.com>
2024-07-07 14:03:11 -0600

    added gravity to galvanized square steel

--- src/main/java/phase/galvanized_veneers/ModBlocks/GalvanizedSteelDefault.java
+++ src/main/java/phase/galvanized_veneers/ModBlocks/GalvanizedSteelDefault.java
@@ -1,7 +1,9 @@
 package phase.galvanized_veneers.ModBlocks;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
@@ -11,16 +13,21 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import phase.galvanized_veneers.GalvanizedVeneers;
 
-public class GalvanizedSteelDefault extends Block {
+public class GalvanizedSteelDefault extends FallingBlock {
+
 
     public GalvanizedSteelDefault(Settings settings) {
         super(settings);
     }
 
-
     @Override
     protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         GalvanizedVeneers.LOGGER.info(String.valueOf(stack));
         return super.onUseWithItem(stack, state, world, pos, player, hand, hit);
+    }
+
+    @Override
+    protected MapCodec<? extends FallingBlock> getCodec() {
+        return null;
     }
 }
