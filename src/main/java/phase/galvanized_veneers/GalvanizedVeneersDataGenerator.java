50a83831d1bb980fb0ddcb4c926d4a368e6f8f28
ThePhaseCat <84151006+ThePhaseCat@users.noreply.github.com>
2024-10-23 16:13:51 -0600

    stuff

--- src/main/java/phase/galvanized_veneers/GalvanizedVeneersDataGenerator.java
+++ src/main/java/phase/galvanized_veneers/GalvanizedVeneersDataGenerator.java
@@ -4,9 +4,6 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import phase.galvanized_veneers.datagen.BlockLootTableGen;
 import phase.galvanized_veneers.datagen.BlockTagGen;
-import phase.galvanized_veneers.datagen.RecipeGen;
-import phase.galvanized_veneers.datagen.LanguageGen;
-import phase.galvanized_veneers.datagen.ItemTagGen;
 
 public class GalvanizedVeneersDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -14,7 +11,6 @@ public class GalvanizedVeneersDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack myPack = fabricDataGenerator.createPack();
 		myPack.addProvider(BlockLootTableGen::new);
 		myPack.addProvider(BlockTagGen::new);
-		myPack.addProvider(RecipeGen::new);
 		//myPack.addProvider(LanguageGen::new);
 		//myPack.addProvider(ItemTagGen::new);
 	}
--- src/main/java/phase/galvanized_veneers/ModBlocks/GalvanizedSteelDefault.java
+++ src/main/java/phase/galvanized_veneers/ModBlocks/GalvanizedSteelDefault.java
@@ -1,23 +1,18 @@
 package phase.galvanized_veneers.ModBlocks;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import phase.galvanized_veneers.GalvanizedVeneers;
-
-import static net.fabricmc.fabric.api.networking.v1.ServerLoginNetworking.getServer;
 
 public class GalvanizedSteelDefault extends FallingBlock {
 
@@ -27,23 +22,23 @@ public class GalvanizedSteelDefault extends FallingBlock {
     }
 
     @Override
-    protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    protected ActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         //GalvanizedVeneers.LOGGER.info(String.valueOf(stack));
         //now replace this block with true galvanized square steel
         if(world.isClient) {
-            return ItemActionResult.SUCCESS;
+            return ActionResult.SUCCESS;
         }
         else{
             //check what player is holding expansion screw
             if(stack.getItem() != GalvanizedVeneers.EXPANSION_SCREW){
                 //GalvanizedVeneers.LOGGER.info("nu uh uh");
-                return ItemActionResult.FAIL;
+                return ActionResult.FAIL;
             }
             //GalvanizedVeneers.LOGGER.info("yum");
             world.playSound(null, pos, SoundEvents.BLOCK_ANVIL_PLACE, SoundCategory.BLOCKS, 1.0f, 1.0f);
             world.setBlockState(pos, GalvanizedVeneers.TRUE_GALVANIZED_SQUARE_STEEL.getDefaultState());
             stack.decrement(1);
-            return ItemActionResult.SUCCESS;
+            return ActionResult.SUCCESS;
         }
 
     }
